8960f73b3c936977bdc44fbc00159dc531aa7a3e
yeoneei <soso3786@gmail.com>
2019-12-26 00:20:34 +0900

    1767 49/ 50

--- SWEA/1767.cpp
+++ SWEA/1767.cpp
@@ -9,29 +9,23 @@
 #include <iostream>
 #include <queue>
 #include <stdio.h>
-#define MAX 12
+#define MAX 13
 using namespace std;
-
-int map[MAX][MAX];
 int T,n;
+bool map[MAX][MAX];
 struct Node{
     int y;
     int x;
-    Node(int y,int x):y(y),x(x){};
+    Node(int y, int x):y(y),x(x){}
 };
-struct Result{
-    int c;
-    int r;
-    Result(int c, int r):c(c),r(r){};
-};
-struct cmp{
-    bool operator()(Result t1, Result t2){
-        if(t1.c < t2.c){
+struct Cmp{
+    bool operator()(Node n1, Node n2){
+        if(n1.y < n2.y){
             return true;
-        }else if(t1.c > t2.c){
+        }else if(n1.y > n2.y){
             return false;
-        }else {
-            if(t1.r > t2.r){
+        }else{
+            if(n1.x > n2.x){
                 return true;
             }else{
                 return false;
@@ -41,95 +35,92 @@ struct cmp{
 };
 
 queue<Node> qu;
-priority_queue<Result, vector<Result>,cmp> pq;
+priority_queue<Node, vector<Node>,Cmp> pq;
 
-
-int maxScore=1000;
-int dx[4]={1,-1,0,0};
-int dy[4]={0,0,1,-1};
-
-int checkScore(int map[][MAX]){
-    int result = 0;
+int checkScore(bool map[][MAX]){
+    int result=0;
     for(int i=0; i<n;i++){
         for(int j=0; j<n;j++){
-            if(map[i][j]==1){
+            if(map[i][j]){
                 result++;
             }
         }
     }
-    cout<<"result" <<result<<endl;
     return result-n;
 }
 
-void dfs(queue<Node> qu, int map[][MAX]){
+int dx[4]={1,-1,0,0};
+int dy[4]={0,0,1,-1};
+
+void dfs(queue<Node> qu, bool map[][MAX]){
     if(qu.empty()){
         return;
     }
     
-    
-    int copy[MAX][MAX];
     int y = qu.front().y;
     int x = qu.front().x;
     qu.pop();
-    printf("%d %d \n",y,x);
-
+    bool copy[MAX][MAX];
     for(int i=0; i<4;i++){
-        for(int j = 0; j<n;j++){
-            for(int k =0; k<n;k++){
+        for(int j=0; j<n;j++){
+            for(int k=0; k<n;k++){
                 copy[j][k]=map[j][k];
-                printf("%d ",copy[j][k]);
             }
-            printf("\n");
         }
-        printf("\n");
         int ny = y;
         int nx = x;
-        bool checkS=true;
+        bool check = true;
         while(1){
-            nx+=dx[i];
             ny+=dy[i];
-            if(nx<0 || nx>=n || ny<0 || ny>n){
+            nx+=dx[i];
+            if(ny<0 || ny>=n ||nx<0 || nx>=n){
                 break;
             }
-            if(copy[ny][nx]==1){
-                checkS = false;
+            if(copy[ny][nx]){
+                check = false;
                 break;
             }
             copy[ny][nx]=1;
         }
         
-        if(checkS){
-            pq.push(Result((n-(int)qu.size()),checkScore(copy)));
-            dfs(qu, copy);
+        if(check){
+            pq.push(Node(n-(int)qu.size(),checkScore(copy)));
+            dfs(qu,copy);
         }
     }
 }
 
+void init(){
+    while(!qu.empty()){
+        qu.pop();
+    }
+    while(!pq.empty()){
+        pq.pop();
+    }
+}
+
 int main(){
-    freopen("/Users/yeoni/Desktop/test/backjoon/SWEA/1767_input.txt", "r", stdin);
-    scanf("%d",&T);
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin>>T;
     for(int t=1; t<=T;t++){
-        scanf("%d",&n);
+        cin>>n;
+        init();
         for(int i=0; i<n;i++){
             for(int j=0; j<n;j++){
-                scanf("%d", &map[i][j]);
-                if(map[i][j]==1){
-                    if(i==0 || j==0 || i==n-1 || j==n-1){
-                        map[i][j]=1;
-                    }else{
+                cin>>map[i][j];
+                if(map[i][j]){
+                    if(i!=0 || j!=0 || i!=n-1 || j!=n-1){
                         qu.push(Node(i,j));
-                        map[i][j]=1;
                     }
                 }
             }
         }
+        
         dfs(qu,map);
-        printf("#%d %d\n",t,pq.top().r);
-        while(!pq.empty()){
-            printf("%d %d\n",pq.top().c,pq.top().r);
-            pq.pop();
-        }
-       
+        printf("#%d %d \n",t,pq.top().x);
         
     }
+    
 }
